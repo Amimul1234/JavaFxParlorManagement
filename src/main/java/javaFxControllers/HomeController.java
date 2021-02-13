d6434474fad82a63cb346803832686fea85dcd78
Amimul <amimulahsan7@gmail.com>
2021-02-13 23:45:22 +0600

    Choice box functional now.

--- src/main/java/javaFxControllers/HomeController.java
+++ src/main/java/javaFxControllers/HomeController.java
@@ -2,6 +2,8 @@ package javaFxControllers;
 
 import dbOperations.DbServices;
 import java.io.IOException;
+import java.util.List;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,55 +17,76 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HomeController {
-
+    
     DbServices dbServices = DbServices.getInstance(); //Get the database service class
 
     @FXML
     private TextField appointmentMakerName;
-
+    
     @FXML
     private TextField appointmentMakerEmail;
-
+    
     @FXML
     private ChoiceBox<String> selectedService;
-
+    
     @FXML
     private Text selectServices;
-
+    
     @FXML
     private DatePicker appointmentDate;
-
+    
     @FXML
     private TextField appointmantMakerPhone;
-
+    
     @FXML
     private ChoiceBox<String> selectedService1;
-
+    
     @FXML
     private Text selectAppointmentTimeText;
-
+    
     @FXML
     public void initialize() {
         //Firing new thread to keep UI safe from hanging 
         new Thread(new Runnable() {
-
+            
             @Override
             public void run() {
-
+                final List<String> servicesNamesList = dbServices.getServicesList();
+                
+                if (servicesNamesList != null) {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            selectedService.getItems().addAll(servicesNamesList);
+                            selectedService.setValue(servicesNamesList.get(0)); //Add the first item as a default value
+                            selectServices.setVisible(false);
+                        }
+                    });
+                } else {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            showAlert(Alert.AlertType.ERROR, "Can not get services", "Can not get services,please try again");
+                        }
+                    });
+                    
+                }
             }
         }
         ).start();
     }
-
+    
     @FXML
-    void aboutUs(ActionEvent event) {
-
+    void aboutUs(ActionEvent event
+    ) {
+        
     }
-
+    
     @FXML
-    void admin(ActionEvent event) {
+    void admin(ActionEvent event
+    ) {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxmls/AdminServiceAdd.fxml"));
-
+        
         Stage stage = (Stage) selectServices.getScene().getWindow();
         Scene scene = null;
         try {
@@ -73,27 +96,31 @@ public class HomeController {
         }
         stage.setScene(scene);
     }
-
+    
     @FXML
-    void contactUs(ActionEvent event) {
-
+    void contactUs(ActionEvent event
+    ) {
+        
     }
-
+    
     @FXML
-    void homeScreen(ActionEvent event) {
-
+    void homeScreen(ActionEvent event
+    ) {
+        
     }
-
+    
     @FXML
-    void services(ActionEvent event) {
-
+    void services(ActionEvent event
+    ) {
+        
     }
-
+    
     @FXML
-    void makeAnAppointment(ActionEvent event) {
+    void makeAnAppointment(ActionEvent event
+    ) {
         validateAppointmentInput();
     }
-
+    
     private void validateAppointmentInput() {
         if (appointmentMakerName.getText().isEmpty()) {
             showAlert(Alert.AlertType.ERROR, "Appointment maker name can not be empty", "Please enter your name");
@@ -103,7 +130,7 @@ public class HomeController {
             showAlert(Alert.AlertType.ERROR, "Appoint maker phone number can not be empty", "Please enter your mobile number");
         }
     }
-
+    
     private static void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
