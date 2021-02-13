678dd7fb02a09ae0839d3b3351f5d1251ccae816
Amimul <amimulahsan7@gmail.com>
2021-02-13 21:13:40 +0600

    Home and admin Services added

---
+++ src/main/java/dbOperations/DbServices.java
@@ -0,0 +1,49 @@
+package dbOperations;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import pojos.Appointment;
+import pojos.Service;
+
+public class DbServices {
+
+    private static DbServices dbServices = null;
+    private final EntityManager entityManager;
+
+    private DbServices() {
+        entityManager = Persistence.createEntityManagerFactory("rahi").createEntityManager();
+    }
+
+    public static DbServices getInstance() //Making this service class singleton
+    {
+        if (dbServices == null) {
+            dbServices = new DbServices();
+        }
+        return dbServices;
+    }
+
+    //Appointment operations
+    public synchronized String addAnAppoinment(Appointment appointment) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(appointment);
+            entityManager.getTransaction().commit();
+            return "Successfully made an appoinment. Appointment number is: " + String.valueOf(appointment.getAppointmentId());
+        } catch (Exception e) {
+            return "Can not make an appointment, please try again";
+        }
+    }
+
+    //Adding new service
+    public synchronized String addNewService(Service service) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(service);
+            entityManager.getTransaction().commit();
+            return "Successfully added new service";
+        } catch (Exception e) {
+            return "Can not add new service, please try again";
+        }
+    }
+
+}
